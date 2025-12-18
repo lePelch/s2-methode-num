8bb2bbfc6e2a8e42699764c2d4e3ece13e74fd4e
le007bigpapa <le007bigpapa@gmail.com>
2025-12-17 21:12:49 -0500

    Interpolation

--- eq_norm.py
+++ eq_norm.py
@@ -1,5 +1,22 @@
 import numpy as np
 
+def poly_interpol_eq_norm(x, y):
+    # Validation
+    if len(x) != len(y):
+        raise ValueError(f"x et y pas la même taille")
+    M = len(x)
+    Phi = np.zeros((M, M))
+    for i in range(M):
+        p = 1
+        for j in range(M):
+            Phi[i, j] = p
+            p *= x[i]
+
+    print(Phi)
+    print(y)
+    A = np.linalg.solve(Phi, y)
+    return A
+
 
 def poly_approx_eq_norm(x, y, deg):
     # Validation
@@ -29,6 +46,7 @@ def fct_from_coeff(A, xn):
     return fx
 
 if __name__ == "__main__":
+    print("Approximation")
     xn = np.array([1, 3, 4, 6, 7])
     yn = np.array([-1.6, 4.8, 6.1, 14.6, 15.1])
     for d in range(1, len(xn) - 1):
@@ -43,8 +61,14 @@ if __name__ == "__main__":
         RMSE = np.sqrt((1/N*err))
         print(f"Le RMSE est de {RMSE}")
 
-    print("\n\n\n")
+    print("\nLes points sont :")
     for x, y in zip(xn, yn):
         print(f"({x}, {y})")
 
+    print("\nInterpolation")
+    # Eq theorique DESMOS -3-0.5x+1.5x^{2}-0.15x^{3}
+    x = np.array([1, 3, 4, 6])
+    y = np.array([-2.15, 4.95, 9.4, 15.6])
+    A = poly_interpol_eq_norm(x, y)
+    print(f"Les coef sont de \n {A}")
 
